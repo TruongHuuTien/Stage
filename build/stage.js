6716a24ebb07322682d4e45924b7e68fd73ff1b2
Tien <truong.htien@gmail.com>
2018-02-01 14:24:26 -0500

    Add compiled file

---
+++ build/stage.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Stage = function Stage(element) {
+  _classCallCheck(this, Stage);
+
+  this.ctx = element.getContext('webgl');
+};
+
+exports.default = Stage;
